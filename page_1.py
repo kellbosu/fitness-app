381db4f4e11ce815e7867624896bc95ec3d654b3
Brandon <kellb@oregonstate.edu>
2026-04-25 07:32:29 -0400

    Style(button): change button color

--- page_1.py
+++ page_1.py
@@ -4,3 +4,15 @@ st.title("ENTER MAXES HERE")
 st.write("ENTER MAXES HERE")
 col1, col2, col3 = st.columns(3)
 col2.button("New Program", width="stretch")
+
+st.markdown("""
+<style>
+    .stButton>button {
+        background-color: #FABC02;
+        color: DarkOrchid;
+        border-radius: 10px;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+st.button("Styled Button")
